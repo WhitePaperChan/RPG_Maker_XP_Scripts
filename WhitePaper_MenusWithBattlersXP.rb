a4a46f2ed02ef6b8e6f60708a2930396dea4173b
WhitePaperChan <47882282+WhitePaperChan@users.noreply.github.com>
2023-04-07 18:20:32 +0300

    Added the text of license for clarification (terms are the same)

--- WhitePaper_MenusWithBattlersXP.rb
+++ WhitePaper_MenusWithBattlersXP.rb
@@ -1,12 +1,30 @@
 # =============================================================================
 # WhitePaper_MenusWithBattlersXP
 # =============================================================================
-# Замінює усю графіку персонажів у вікнах на бойову графіку.
+# Замінює усю графіку персонаж_ок у вікнах на бойову графіку.
 # 
 # Технічні подробиці:
 # Цей скрипт змінює метод draw_actor_graphic класу Window_Base.
 #
 # Цей скрипт розповсюджується за ліцензією MIT-0 (MIT No Attribution License)
+# MIT No Attribution
+#
+# Copyright 2022-2023 WhitePaperChan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE. 
 # =============================================================================
 
 # =============================================================================
